897ace313219602237d4d3cff4255ac283dc8db8
Puneeth2004 <puneethk7013@gmail.com>
2024-04-22 18:17:43 +0530

    Added

---
+++ SumOfNumbers1.java
@@ -0,0 +1,9 @@
+public class SumOfNumbers1  
+{  
+public static void main(String args[])   
+{  
+int n1 = 18, n2 =17 , sum;  
+sum = n1 + n2;  
+System.out.println("The sum of numbers is: "+sum);  
+}  
+}  
